803e70cd494f22cf65ae756700b2a05b3a82b4a6
InnocentOgero <innocent.ogero@student.moringaschool.com>
2023-09-18 23:52:23 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  let message;
+  if(feet<=400){
+    message='This one is on me!';
+    return message
+  }
+  else if(feet>2000 && feet<=2500){
+    message='I will gladly take your thirty bucks.';
+    return message
+  }
+  else {
+    message='No can do.';
+    return message
+  }
+}
+scuberGreetingForFeet(199)
+function ternaryCheckCity(city){
+  // Write your code here! 
+  let message;
+  city=='NYC'?message="Ok, sounds good.":message="No go.";
+  return message
+  
+
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
+  let message;
+  switch(tip){
+    case 'generous':
+      message="Thank you so much.";
+      return message
+      break;
+    case 'not as generous':
+      message='Thank you.';
+      return message
+      break;
+    default:
+      message="Bye."
+      return message 
+      break;
+  }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
 }
